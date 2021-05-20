fdd250bfbeb5686d078f96699c2cb842ad451bc8
Louis Wong <louis.d.wong@oracle.com>
2021-05-20 11:30:29 -0700

    DC

---
+++ LeetCode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<TreeNode> next = new ArrayList<>();
+        next.add(root);
+        while (!next.isEmpty()) {
+            List<TreeNode> temp = new ArrayList<>();
+            for (TreeNode node : next) {
+                if (node.left != null) {
+                    temp.add(node.left);
+                }
+                if (node.right != null) {
+                    temp.add(node.right);
+                }
+            }
+            res.add(next.stream().map(n -> n.val).collect(Collectors.toList()));
+            next = temp;
+        }
+        return res;
+    }
+}
