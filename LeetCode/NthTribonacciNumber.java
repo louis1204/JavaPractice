871a89752912ddbfefa736fa80c92cf39e60ef6f
Louis Wong <louis.d.wong@oracle.com>
2021-09-24 10:02:51 -0700

    DC

---
+++ LeetCode/NthTribonacciNumber.java
@@ -0,0 +1,21 @@
+class Solution {
+    HashMap<Integer, Integer> cache = new HashMap<>();
+    
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+        int res = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        cache.put(n, res);
+        return res;
+    } 
+}
