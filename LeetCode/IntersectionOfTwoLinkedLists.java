cae80eb24937efc82be8d6f8f7745d535c0b1c5b
Louis <Louis1204@gmail.com>
2022-06-05 18:34:25 -0700

    DC

--- LeetCode/IntersectionOfTwoLinkedLists.java
+++ LeetCode/IntersectionOfTwoLinkedLists.java
@@ -25,3 +25,41 @@ public class Solution {
         return null;
     }
 }
+
+// The O(1) memory restriction. Runtime is a lot faster as well 1ms vs 7ms
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // O(1) memory usage method
+        // Get the lengths of A and B
+        // EG 5, 6, then we need to increment the longer one by the difference
+        // then increment both until they're both pointing at the same node
+        int aSize = 0;
+        ListNode tempA = headA;
+        while (tempA != null) {
+            aSize++;
+            tempA = tempA.next;
+        }
+        
+        int bSize = 0;
+        ListNode tempB = headB;
+        while (tempB != null) {
+            bSize++;
+            tempB = tempB.next;
+        }
+        
+        if (aSize > bSize) {
+            for (int i = 0; i < aSize - bSize; i++) {
+                headA = headA.next;
+            }
+        } else if (bSize > aSize) {
+            for (int i = 0; i < bSize - aSize; i++) {
+                headB = headB.next;
+            }
+        }
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+}
