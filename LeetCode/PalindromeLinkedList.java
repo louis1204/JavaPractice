f04491c9abc94bb91fd9857b69b7449d4fcd8f22
Louis Wong <wlouis@vmware.com>
2022-08-23 19:47:46 -0700

    DC

---
+++ LeetCode/PalindromeLinkedList.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode ptr = head;
+        Stack<ListNode> stack = new Stack<>();
+        while (ptr != null) {
+            stack.push(ptr);
+            ptr = ptr.next;
+        }
+        while (head != null) {
+            if (head.val != stack.pop().val) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+}
