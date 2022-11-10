788bfff0f5a166213d21a4a05d9a9c81d36a9027
Louis Wong <wlouis@vmware.com>
2022-11-10 14:36:51 -0800

    DC

---
+++ LeetCode/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        // Monotonic stack
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
+    }
+}
